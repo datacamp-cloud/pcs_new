0253098b9c733f23abe2cd525ac0d6b19fe9572e
datacamp-cloud <campbellsehebi04@gmail.com>
2025-07-13 01:10:28 +0000

    update

--- backend/utils/normalizePhone.js
+++ backend/utils/normalizePhone.js
@@ -1,4 +1,6 @@
 // utils/normalizePhone.js
 module.exports = function normalizePhone(phone) {
-  return phone.replace(/^(\+225|00225)/, '').replace(/\s/g, '');
+  return phone
+    .replace(/^(\+225|00225)/, '')
+    .replace(/\s/g, '');
 };
